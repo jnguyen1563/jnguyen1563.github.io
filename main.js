e941b9f565fcc768b54fa2e8143c331786e8287d
jgnuyen1563 <justinnguyen0821@gmail.com>
2021-02-02 00:31:40 -0800

    Created observer to change how navbar interacts

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-const header = document.querySelector('header');
 const navbar = document.querySelector('.navbar');
 const sectionOne = document.querySelector('.intro');
 
 const sectionOneOptions = { 
-    rootMargin: "-600px 0px 0px 0px"
+    rootMargin: "-650px 0px 0px 0px"
 };
 
 const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver) {
     entries.forEach(entry => {
         if(!entry.isIntersecting) {
-            navbar.classList.add("navbar-scroll");
+            navbar.classList.add("navbar-invert");
         } else {
-            navbar.classList.remove("navbar-scroll");
+            navbar.classList.remove("navbar-invert");
         }
     })
 }, sectionOneOptions);
